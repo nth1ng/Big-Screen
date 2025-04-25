0c5f142dade845d84e55552e51ec76539788f548
武俊言 <15034944319@qq.com>
2025-04-25 22:18:21 +0800

    first

---
+++ src/views/index/right-center.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import CapsuleChart from "@/components/datav/capsule-chart";
+import { ranking } from "@/api";
+import { ElMessage } from "element-plus";
+
+const config = ref({
+  showValue: true,
+  unit: "个",
+});
+const data = ref([]);
+const getData = () => {
+  ranking()
+    .then((res) => {
+      console.log("右中--报警排名", res);
+      if (res.success) {
+        data.value = res.data;
+      } else {
+        ElMessage({
+          message: res.msg,
+          type: "warning",
+        });
+      }
+    })
+    .catch((err) => {
+      ElMessage.error(err);
+    });
+};
+getData();
+</script>
+
+<template>
+  <div class="right_bottom">
+    <CapsuleChart :config="config" style="width: 100%; height: 260px; margin: 0 auto;" :data="data" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.right_bottom {
+  box-sizing: border-box;
+  padding: 16px;
+  padding-top: 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
